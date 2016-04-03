4d15fe9fab6cbd9b34dd838da5cf927c07caac90
Jeremy Zilar <jeremyzilar@gmail.com>
2016-04-03 10:38:49 -0400

    header run fix

--- inc/head.php
+++ inc/head.php
@@ -1,20 +1,23 @@
 <header>
   <div class="container">
     <div class="row">
-      <div id="hed" class="col-xs-12 col-sm-6">
-        <h1>
-          <a href="<?php echo esc_url( home_url( '/' ) ); ?>" title="<?php echo esc_attr( get_bloginfo( 'name', 'display' ) ); ?>" rel="home"><?php echo esc_attr( get_bloginfo( 'name', 'display' ) ); ?></a>
-          </a>
-        </h1>
-        <?php if (is_home()) { ?>
-        <h2><?php echo esc_attr( get_bloginfo( 'description' ) ); ?></h2>
-        <?php } ?>
-        
-        <?php include(INC . 'site-nav.php'); ?> 
-
+      <div class="col-xs-12">
+        <div class="wrap">
+          <div class="row">
+            <div id="hed" class="col-xs-12 col-sm-8">
+              <h1>
+                <a href="<?php echo esc_url( home_url( '/' ) ); ?>" title="<?php echo esc_attr( get_bloginfo( 'name', 'display' ) ); ?>" rel="home"><?php echo esc_attr( get_bloginfo( 'name', 'display' ) ); ?></a>
+                </a>
+              </h1>
+              <?php if (is_home()) { ?>
+              <h2><?php echo esc_attr( get_bloginfo( 'description' ) ); ?></h2>
+              <?php } ?>
+              
+              <?php include(INC . 'site-nav.php'); ?> 
+            </div>
+          </div>
+        </div>
       </div>
     </div>
-    
-   
   </div>
 </header>
